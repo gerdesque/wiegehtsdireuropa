e57932a86e00358d5174ecd2417e89d7c1854334
Gerd Müller <gerdesk@posteo.org>
2019-04-21 17:04:43 +0200

    Add duotone effect for polaroids

--- src/Image.js
+++ src/Image.js
@@ -6,15 +6,22 @@ class Image extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      src: require('./assets/' + this.props.id + '.jpg')
+      src: require('./assets/' + this.props.id + '.jpg'),
+      color: ["", "yellow-red","green-blue", "purple-green"]
     };
+  }
+
+  getRandomColor(max) {
+    return Math.floor(Math.random() * Math.floor(max));
   }
 
   render() {
 
     return (
-      <div className="Image-container" title={this.props.sentiments} >
-        <img className="Image" {...this.props} src={this.state.src} alt={this.props.id}/>
+      <div className="Image-container" title={this.props.sentiments}>
+        <div className={this.state.color[this.getRandomColor(4)] + ' duotone'}>
+          <img className="Image" {...this.props} src={this.state.src} alt={this.props.id}/>
+        </div>
       </div>
     )
   }
