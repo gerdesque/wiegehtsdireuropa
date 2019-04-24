0633793cff2a27fd477e8740e83ee7d0cef3a303
Gerd Müller <gerdesk@posteo.org>
2019-04-24 19:47:36 +0200

    Convert images to webp

--- src/Image.js
+++ src/Image.js
@@ -7,7 +7,7 @@ class Image extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      src: require('./assets/squares/' + this.props.id + '.jpg'),
+      src: require('./assets/squares/' + this.props.id + '.webp'),
       color: ["", "yellow-red","green-blue", "purple-green"]
     };
   }
@@ -21,7 +21,7 @@ class Image extends Component {
     return (
       <div className="Image-container" title={this.props.sentiments}>
         <div className={'duotone ' + this.state.color[this.getRandomColor(4)]}>
-        <LazyImage className="Image" offset="0" {...this.props} link={this.state.src} alt={this.props.id}/>
+        <LazyImage className="Image" offset={100} {...this.props} link={this.state.src} alt={this.props.id}/>
         </div>
       </div>
     )
