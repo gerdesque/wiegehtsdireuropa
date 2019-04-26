d70fa6d29f5a3f5787535cc5ecb49d116d22def5
Gerd Müller <gerdesk@posteo.org>
2019-04-26 13:33:43 +0200

    Adjust header layout

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import './App.css';
 import ImageList from './ImageList';
 import Boxes from './Boxes';
 
+const small = require('./assets/hero_360.webp');
+const medium = require('./assets/hero_853.webp');
+const large = require('./assets/hero_1246.webp');
+const mega = require('./assets/hero_1901.webp');
+const giga = require('./assets/hero_1920.webp');
+
+const ResponsiveImage = () => (
+  <img sizes="(max-width: 1920px) 100vw, 1920px" src={giga} alt="header"
+    srcSet={`${small} 360w, ${medium} 853w, ${large} 1246w, ${mega} 1901w, ${giga} 1920w`}/>
+);
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -27,16 +38,21 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img className="App-logo" src={logo} alt="logo"/>
-          <h1>Wie geht's dir, Europa?</h1>     
+      <>
+        <header>
+          <div className="App-logo">
+            <img src={logo} alt="logo"/>
+          </div>
+          <div className="App-text">
+            <h1>Wie geht's dir, Europa?</h1>
+          </div>
         </header>
-          <div className="App-hero"/>
+        <main>
           <ImageList images={this.state.images}/>
           <Boxes assets={this.state.tweets}/>
           <Boxes assets={this.state.hashtags}/>
-      </div>
+        </main>
+      </>
     );
   }
 }
